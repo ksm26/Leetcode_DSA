cb967a811fc6567ea17f866c4ffd9446380142f2
Khushdeep-singh <khushdeepvnit@gmail.com>
2026-03-27 20:53:23 +0100

    Time: 0 ms (100.00%) | Memory: 19.1 MB (92.24%) - LeetSync

--- 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.py
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.py
@@ -1,10 +1,15 @@
 class Solution:
     def repeatedCharacter(self, s: str) -> str:
-        hashmap = {}
+        # hashmap = {}
+        # for i in s : 
+        #     if i in hashmap.keys():
+        #         return i 
+        #     hashmap[i] = 1
+        hashmap = set()
         for i in s : 
-            if i in hashmap.keys():
+            if i in hashmap:
                 return i 
-            hashmap[i] = 1
+            hashmap.add(i)
 
             
             
