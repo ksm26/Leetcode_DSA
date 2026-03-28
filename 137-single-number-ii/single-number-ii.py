d8e5ebc08d217a1a99eecb21361e654d18536102
Khushdeep-singh <khushdeepvnit@gmail.com>
2026-03-28 22:53:19 +0100

    Time: 3 ms (72.44%) | Memory: 20.7 MB (55.43%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hashmap = {}
+        for i in nums :
+            if i in hashmap :
+                hashmap[i] += 1 
+            else : 
+                hashmap[i] = 1
+
+        for i in nums :
+            if hashmap[i] == 1 :
+                return i 
