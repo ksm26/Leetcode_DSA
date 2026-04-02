f6d3214539401d5a478791abb87aa7ecd03d1338
Khushdeep-singh <khushdeepvnit@gmail.com>
2026-04-02 22:31:52 +0200

    Time: 2 ms (39.45%) | Memory: 19.3 MB (79.84%) - LeetSync

--- 1098-largest-unique-number/largest-unique-number.py
+++ 1098-largest-unique-number/largest-unique-number.py
@@ -2,13 +2,11 @@ from collections import Counter
 class Solution:
     def largestUniqueNumber(self, nums: List[int]) -> int:
         numdict = Counter(nums)
-        numones = []
+        maxunique = -1
 
         for key in numdict : 
             if numdict[key] == 1 :
-                numones.append(key)
-        if not numones : 
-            return -1 
+                maxunique = max(maxunique,key)
 
-        return sorted(numones)[-1] 
+        return maxunique 
         
