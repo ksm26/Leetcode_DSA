dad5565760ec006cf0bd3589d709fc391badae4c
Khushdeep-singh <khushdeepvnit@gmail.com>
2026-04-04 09:17:18 +0200

    Time: 75 ms (12.14%) | Memory: 30.8 MB (25.86%) - LeetSync

---
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
@@ -0,0 +1,29 @@
+from collections import Counter
+class Solution:
+    def dividePlayers(self, skill: List[int]) -> int:
+        total = sum(skill)
+        chemistry = 0 
+
+        if total % (len(skill) // 2) != 0:
+            return -1
+
+        score = total //(len(skill)//2)
+
+        numdict = Counter(skill)
+
+        for i in skill:
+            if numdict[i] == 0 :
+                continue
+            if i == score- i and numdict[i] < 2 :
+                return -1 
+
+            if numdict[score-i] == 0 :
+                return -1 
+            numdict[i] -= 1 
+            numdict[score-i] -= 1
+
+            chemistry += i * (score-i) 
+        
+        
+        return chemistry
+        
