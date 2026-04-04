1043dc64ff145fc7ff4bb85950b9f0e218e2b439
Khushdeep-singh <khushdeepvnit@gmail.com>
2026-04-04 17:36:56 +0200

    Time: 64 ms (93.49%) | Memory: 37.7 MB (47.11%) - LeetSync

--- 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.py
+++ 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.py
@@ -2,16 +2,28 @@ from collections import defaultdict
 class Solution:
     def minimumCardPickup(self, cards: List[int]) -> int:
         
-        cardict = defaultdict(list)
+        # cardict = defaultdict(list)
+        # count = float("inf")
+        # for i in range(len(cards)): # O(n)
+        #     num = cards[i]
+        #     cardict[num].append(i)
+
+        # for key in cardict :  # O(k)
+        #     arr = cardict[key]
+        #     for i in range(len(arr)-1):  # O(m)
+        #         count = min(count, arr[i+1]-arr[i] + 1 )
+
+        # return count if count < float("inf") else -1  # O(k*m)
+
+        cardict = {}
         count = float("inf")
         for i in range(len(cards)):
             num = cards[i]
-            cardict[num].append(i)
-
-        for key in cardict :
-            arr = cardict[key]
-            for i in range(len(arr)-1):
-                count = min(count, arr[i+1]-arr[i] + 1 )
+            if num in cardict:
+                count = min(count, i - cardict[num] + 1)
+                cardict[num] = i 
+            else : 
+                cardict[num] = i 
 
         return count if count < float("inf") else -1
 
