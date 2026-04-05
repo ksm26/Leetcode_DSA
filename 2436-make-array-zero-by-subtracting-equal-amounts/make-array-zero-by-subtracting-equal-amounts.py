0b810dbb45a7392e9cd8a18c1676b705efbcdbe4
Khushdeep-singh <khushdeepvnit@gmail.com>
2026-04-06 00:38:45 +0200

    Time: 0 ms (100.00%) | Memory: 19.2 MB (91.55%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.py
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.py
@@ -1,10 +1,11 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
         
-        seen = set(nums)
+        seen = set(nums) # O(n)
         count = 0 
-        for i in seen:
-            if i != 0 :
-                count += 1 
+        # for i in seen: # O(n)
+        #     if i != 0 :
+        #         count += 1 
 
-        return count
+        # return count
+        return len((seen)  - {0})
