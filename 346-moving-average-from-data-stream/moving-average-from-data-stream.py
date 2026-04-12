8743bf51c3fbd6e0708a0aaf210de6a293e3cdcb
Khushdeep-singh <khushdeepvnit@gmail.com>
2026-04-12 20:55:08 +0200

    Time: 7 ms (54.18%) | Memory: 22.3 MB (51.85%) - LeetSync

---
+++ 346-moving-average-from-data-stream/moving-average-from-data-stream.py
@@ -0,0 +1,24 @@
+from collections import deque
+class MovingAverage:
+
+    def __init__(self, size: int):
+        self.queue = deque()
+        self.size = size
+        self.curr_sum = 0
+
+    def next(self, val: int) -> float:
+
+        self.queue.append(val)
+        self.curr_sum += val
+        if len(self.queue) > self.size :
+            removed = self.queue.popleft()
+            self.curr_sum -= removed
+        
+        return self.curr_sum / len(self.queue)
+
+        
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
