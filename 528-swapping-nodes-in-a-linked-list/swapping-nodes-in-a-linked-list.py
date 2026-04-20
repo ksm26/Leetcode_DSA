4b1a0921f13a7aa19b4bfa9661e785f78e6db5b4
Khushdeep-singh <khushdeepvnit@gmail.com>
2026-04-20 20:22:23 +0200

    Time: 62 ms (51.62%) | Memory: 53.5 MB (99.70%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.py
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.py
@@ -5,21 +5,32 @@
 #         self.next = next
 class Solution:
     def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        node = head
-        arr = []
+        # node = head
+        # arr = []
 
-        while node:
-            arr.append(node.val)
-            node = node.next
+        # while node:
+        #     arr.append(node.val)
+        #     node = node.next
 
-        arr[k-1] , arr[len(arr)-k] = arr[len(arr)-k], arr[k-1]
+        # arr[k-1] , arr[len(arr)-k] = arr[len(arr)-k], arr[k-1]
 
-        node = head
-        for i in arr:
-            node.val = i
-            node = node.next 
+        # node = head
+        # for i in arr:
+        #     node.val = i
+        #     node = node.next 
 
-        return head
+        # return head
+        fast = last = head
+        for i in range(1,k):
+            fast = fast.next 
+
+        nullptr = fast
+        while nullptr.next:
+            last = last.next
+            nullptr = nullptr.next
+        fast.val, last.val = last.val, fast.val
+        return head 
+
 
         
 
