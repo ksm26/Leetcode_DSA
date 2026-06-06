65a9f17b404d84a6bff7b13ad72f2acee8d0cc46
Khushdeep-singh <khushdeepvnit@gmail.com>
2026-06-06 14:42:18 +0200

    Time: 199 ms (16.29%) | Memory: 19.8 MB (42.77%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -4,13 +4,19 @@ class Solution:
 
         k = len(p)
         ref = Counter(p)
+        wind = Counter()
         ans =  [] 
 
-        for i in range(len(s)-k+1):
-            arr = s[i:i+k]
-            count = Counter(arr)
-            if count == ref:
-                ans.append(i)
+        for i in range(len(s)):
+            wind[s[i]] += 1
+
+            if i >= k : 
+                wind[s[i-k]] -= 1
+                if wind[s[i-k]] == 0 : 
+                    del wind[s[i-k]]
+
+            if wind == ref:
+                ans.append(i-k+1)
             
         return ans
 
