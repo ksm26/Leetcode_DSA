747af5d4acad59e9785a472b37158434e2e28a2a
Khushdeep-singh <khushdeepvnit@gmail.com>
2026-06-07 17:15:46 +0200

    Time: 171 ms (50.63%) | Memory: 20.6 MB (80.68%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,26 +1,46 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
 
-        def k_works(k):
-            hrs = 0 
-            for p in piles : 
-                hrs += math.ceil(p/k)
+        # def k_works(k):
+        #     hrs = 0 
+        #     for p in piles : 
+        #         hrs += math.ceil(p/k)
 
-            return hrs <= h 
-        left = 1 
-        right = max(piles) # max speed of eating possible
+        #     return hrs <= h 
+        # left = 1 
+        # right = max(piles) # max speed of eating possible
 
-        # implement this as a binary search 
-        while left < right :
-            k = (left+right)//2
-            if k_works(k):
-                right = k
-            else : 
-                left = k + 1 
+        # # implement this as a binary search 
+        # while left < right :
+        #     k = (left+right)//2
+        #     if k_works(k):
+        #         right = k
+        #     else : 
+        #         left = k + 1 
 
-        return right 
+        # return right 
 
         # time : O ( n * log(max(piles)))
+
+        def k_works(k):
+            hrs = 0 
+            for p in piles:
+                hrs += math.ceil(p/k)
+
+            return hrs <= h
+
+        left = 1 
+        right = max(piles)
+        while left < right : 
+            k = (left+right)//2
+            if k_works(k):
+                right = k 
+            else : 
+                left = k + 1
+
+        return right
+
+        
 
 
 
